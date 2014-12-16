8b3453c1453cf654ac85a55d2bcac1b58cbdd423
Miguel Aguilera <miguel@maguilera.net>
2014-12-16 17:56:52 +0100

    Change ordering method to use reddit's algorithm

--- drum/links/utils.py
+++ drum/links/utils.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.utils.timezone import now
-
+from math import log10
+import datetime
 
 def order_by_score(queryset, score_fields, date_field, reverse=True):
     """
@@ -17,7 +18,8 @@ def order_by_score(queryset, score_fields, date_field, reverse=True):
     """
 
     scale = getattr(settings, "SCORE_SCALE_FACTOR", 2)
-
+    timeweight = 45000.0
+	
     # Timestamp SQL function snippets mapped to DB backends.
     # Defining these assumes the SQL functions POW() and NOW()
     # are available for the DB backend.
@@ -27,6 +29,7 @@ def order_by_score(queryset, score_fields, date_field, reverse=True):
     }
     db_engine = settings.DATABASES[queryset.db]["ENGINE"].rsplit(".", 1)[1]
     timestamp_sql = timestamp_sqls.get(db_engine)
+    print 'database', timestamp_sql
 
     if timestamp_sql:
         score_sql = "(%s) / POW(%s - %s, %s)" % (
@@ -39,8 +42,15 @@ def order_by_score(queryset, score_fields, date_field, reverse=True):
         return queryset.extra(select={"score": score_sql}).order_by(order_by)
     else:
         for obj in queryset:
-            age = (now() - getattr(obj, date_field)).total_seconds()
-            score_fields_sum = sum([getattr(obj, f) for f in score_fields])
-            score = score_fields_sum / pow(age, scale)
-            setattr(obj, "score", score)
+			seconds = (getattr(obj, date_field).replace(tzinfo=None) - datetime.datetime.fromtimestamp(1134028003)).total_seconds() 
+			s = sum([getattr(obj, f) for f in score_fields])
+			order = log10(max(abs(s), 1))
+			if s > 0:
+				sign = 1
+			elif s < 0:
+				sign = -1
+			else:
+				sign = 0
+			score = round(sign * order + seconds / timeweight, 7)				
+			setattr(obj, "score", score)
         return sorted(queryset, key=lambda obj: obj.score, reverse=reverse)
