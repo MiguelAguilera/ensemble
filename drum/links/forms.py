9532b34634e91a47ddd6b13fa29cc2dc44450e98
Miguel Aguilera <miguel@maguilera.net>
2014-12-27 17:38:23 +0100

    raise error only if description is not present

--- drum/links/forms.py
+++ drum/links/forms.py
@@ -1,4 +1,3 @@
-
 from django.conf import settings
 from django.forms.models import modelform_factory
 from django.forms import ValidationError
@@ -6,14 +5,16 @@ from django.forms import ValidationError
 from drum.links.models import Link
 
 
-BaseLinkForm = modelform_factory(Link, fields=["title", "link", "description"])
-
+BaseLinkForm = modelform_factory(Link, fields=["title", "description"])
+#BaseLinkForm = modelform_factory(Link, fields=["title", "link", "description"])
 
 class LinkForm(BaseLinkForm):
 
     def clean(self):
         link = self.cleaned_data.get("link", None)
         description = self.cleaned_data.get("description", None)
-        if not link and not description:
-            raise ValidationError("Either a link or description is required")
+        if not link:
+            raise ValidationError("Description is required")
+#        if not link and not description:
+#            raise ValidationError("Either a link or description is required")
         return self.cleaned_data
