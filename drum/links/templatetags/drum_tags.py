abc188eb5c2e238cf560e8ea07edef50254bc38b
MiguelAguilera <miguel@maguilera.net>
2014-12-29 14:36:14 +0100

    modify templates

--- drum/links/templatetags/drum_tags.py
+++ drum/links/templatetags/drum_tags.py
@@ -28,3 +28,34 @@ def order_comments_by_score_for(context, link):
 @register.filter
 def short_timesince(date):
     return timesince(date).split(",")[0]
+    
+@register.filter(name='mult')
+def mult(value, arg):
+    "Multiplies the arg and the value"
+    return float(value) * float(arg)
+
+@register.filter(name='div')
+def div(value, arg):
+    "Divides the arg and the value"
+    print value, arg
+    return float(value) / float(arg)
+    
+@register.filter(name='addfloat')
+def addfloat(value, arg):
+    "Sums the arg and the value"
+    print value
+    if not value:
+    	return 0
+    return float(value) + float(arg)
+
+#def sub(value, arg):
+#    "Subtracts the arg from the value"
+#    return int(value) - int(arg)
+
+#def div(value, arg):
+#    "Divides the value by the arg"
+#    return int(value) / int(arg)
+
+#template.register_filter('mult', mult, True)
+#template.register_filter('sub', sub, True)
+#template.register_filter('div', div, True)
