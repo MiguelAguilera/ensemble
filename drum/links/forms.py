6460cec63ef6a9b96e2de969b723b1a2810d95ae
MiguelAguilera <miguel@maguilera.net>
2014-12-30 12:23:15 +0100

    add link field to form

--- drum/links/forms.py
+++ drum/links/forms.py
@@ -5,7 +5,7 @@ from django.forms import ValidationError
 from drum.links.models import Link
 
 
-BaseLinkForm = modelform_factory(Link, fields=["title", "description","keywords"])
+BaseLinkForm = modelform_factory(Link, fields=["title", "description","link"])
 #BaseLinkForm = modelform_factory(Link, fields=["title", "link", "description"])
 
 class LinkForm(BaseLinkForm):
@@ -13,7 +13,7 @@ class LinkForm(BaseLinkForm):
     def clean(self):
         link = self.cleaned_data.get("link", None)
         description = self.cleaned_data.get("description", None)
-        keywords = self.cleaned_data.get("keywords", None)
+#        keywords_string = self.cleaned_data.get("keywords", None)
         if not description:
             raise ValidationError("Description is required")
 #        if not link and not description:
