a0fd6a5ea97a74e0c291d32e7486358c9d913125
Cybersyn <zylantro@cybersyn.cybersyn.maguilera.net>
2015-01-01 14:52:15 +0100

    small appeareance changes

--- ensemble/links/forms.py
+++ ensemble/links/forms.py
@@ -5,15 +5,15 @@ from django.forms import ValidationError
 from ensemble.links.models import Link
 
 
-BaseLinkForm = modelform_factory(Link, fields=["title", "description","link"])
+BaseLinkForm = modelform_factory(Link, fields=["title", "description","keywords"])
 #BaseLinkForm = modelform_factory(Link, fields=["title", "link", "description"])
 
 class LinkForm(BaseLinkForm):
 
     def clean(self):
-        link = self.cleaned_data.get("link", None)
+        #link = self.cleaned_data.get("link", None)
         description = self.cleaned_data.get("description", None)
-#        keywords_string = self.cleaned_data.get("keywords", None)
+        keywords_string = self.cleaned_data.get("keywords", None)
         if not description:
             raise ValidationError("Description is required")
 #        if not link and not description:
