1d9e70292ad248c501c6b730d50f771edf1cf239
MiguelAguilera <miguel@maguilera.net>
2015-01-17 17:59:20 +0100

    change language to spanish

--- ensemble/project_template/settings.py
+++ ensemble/project_template/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "en"
+LANGUAGE_CODE = "es"
 
 # Supported languages
 _ = lambda s: s
