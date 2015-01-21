64e2a088e2e49e1c4c9d32c9b2741a599ef1b14f
MiguelAguilera <miguel@maguilera.net>
2015-01-21 08:53:55 +0100

    Removed hardcoded auth.User reference

--- ensemble/links/models.py
+++ ensemble/links/models.py
@@ -25,6 +25,7 @@ from mezzanine.generic.models import Rating, Keyword, AssignedKeyword
 from mezzanine.generic.fields import RatingField, CommentsField
 from mezzanine.utils.urls import slugify
 
+USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 class Link(Displayable, Ownable):
 
@@ -73,7 +74,7 @@ class Link(Displayable, Ownable):
 
 class Profile(models.Model):
 
-    user = models.OneToOneField("auth.User")
+    user = models.OneToOneField(USER_MODEL)
 #    website = models.URLField(blank=True)
 #    bio = models.TextField(blank=True)
     website = models.URLField(blank=True, editable=False)
